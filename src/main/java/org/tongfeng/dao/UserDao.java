bcc6bac76774a485b7cf1558e8cc8e7821c955e2
liaojiwuyu <liaojiwuyu@gmail.com>
2014-05-13 14:41:14 +0800

    Update UserDao.java

--- src/main/java/org/tongfeng/dao/UserDao.java
+++ src/main/java/org/tongfeng/dao/UserDao.java
@@ -4,6 +4,34 @@ import org.springframework.data.repository.Repository;
 import org.tongfeng.Entity.AccountInfo;
 
 public interface UserDao extends Repository<AccountInfo, Long>{
+	/**
+	 * 查询中的特定词用法
+	Keyword	Sample	JPQL snippet
+	And	findByLastnameAndFirstname	… where x.lastname = ?1 and x.firstname = ?2
+	Or	findByLastnameOrFirstname	… where x.lastname = ?1 or x.firstname = ?2
+	Is,Equals	findByFirstname,findByFirstnameIs,findByFirstnameEquals	… where x.firstname = 1?
+	Between	findByStartDateBetween	… where x.startDate between 1? and ?2
+	LessThan	findByAgeLessThan	… where x.age < ?1
+	LessThanEqual	findByAgeLessThanEqual	… where x.age <= ?1
+	GreaterThan	findByAgeGreaterThan	… where x.age > ?1
+	GreaterThanEqual	findByAgeGreaterThanEqual	… where x.age >= ?1
+	After	findByStartDateAfter	… where x.startDate > ?1
+	Before	findByStartDateBefore	… where x.startDate < ?1
+	IsNull	findByAgeIsNull	… where x.age is null
+	IsNotNull,NotNull	findByAge(Is)NotNull	… where x.age not null
+	Like	findByFirstnameLike	… where x.firstname like ?1
+	NotLike	findByFirstnameNotLike	… where x.firstname not like ?1
+	StartingWith	findByFirstnameStartingWith	… where x.firstname like ?1 (parameter bound with appended %)
+	EndingWith	findByFirstnameEndingWith	… where x.firstname like ?1 (parameter bound with prepended %)
+	Containing	findByFirstnameContaining	… where x.firstname like ?1 (parameter bound wrapped in %)
+	OrderBy	findByAgeOrderByLastnameDesc	… where x.age = ?1 order by x.lastname desc
+	Not	findByLastnameNot	… where x.lastname <> ?1
+	In	findByAgeIn(Collection<Age> ages)	… where x.age in ?1
+	NotIn	findByAgeNotIn(Collection<Age> age)	… where x.age not in ?1
+	True	findByActiveTrue()	… where x.active = true
+	False	findByActiveFalse()	… where x.active = false
+	IgnoreCase	findByFirstnameIgnoreCase	… where UPPER(x.firstame) = UPPER(?1)
+	 * /
 	public AccountInfo save(AccountInfo accountInfo);
 	
 	public AccountInfo findByAccountId(Long accountId);
